947a6a5cc3ae17b0e1b988beb5d57e9738cb5698
svyatoyrandom <mrsteel019@gmail.com>
2021-11-24 00:07:23 +0300

    finish_point field deleted; step, interval, start_point is float now

--- plot/models.py
+++ plot/models.py
@@ -3,10 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Plot(models.Model):
-    start_point = models.IntegerField()
-    finish_point = models.IntegerField()
-    interval = models.PositiveIntegerField(validators=[MinValueValidator(1e-4),])
-    step = models.PositiveIntegerField(validators=[MinValueValidator(1e-4),])
+    start_point = models.FloatField()
+    interval = models.FloatField(validators=[MinValueValidator(1e-4),])
+    step = models.FloatField(validators=[MinValueValidator(1e-4),])
     plot_function = models.TextField(max_length=200)
     date_upload = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
